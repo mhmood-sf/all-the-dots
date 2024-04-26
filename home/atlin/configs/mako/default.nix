cfe8f782ef9c41735f99baa771ea534f9c257f1e
mhmood-sf <28828855+mhmood-sf@users.noreply.github.com>
2024-04-26 17:54:18 -0400

    add mako, tofi configs

---
+++ home/atlin/configs/mako/default.nix
@@ -0,0 +1,12 @@
+{
+  config = {
+    services.mako = {
+      enable = true;
+      anchor = "bottom-right";
+      backgroundColor = "#FAFAFA";
+      borderRadius = 5;
+      borderSize = 0;
+      font = "Open Sans 12";
+    };
+  };
+}
