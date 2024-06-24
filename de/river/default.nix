8bd2766141d7b9eb810b53d703bc478f0d2a21e5
mhmood-sf <28828855+mhmood-sf@users.noreply.github.com>
2024-06-24 04:31:13 -0400

    try to fix riverwm screenshare (wip?)

--- de/river/default.nix
+++ de/river/default.nix
@@ -4,4 +4,16 @@
     enable = true;
     extraPackages = [];
   };
+
+  # Enable XDG Desktop Portals for screensharing (and other stuff?)
+  xdg = {
+    portal = {
+      enable = true;
+      wlr.enable = true;
+
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+      ];
+    };
+  };
 }
