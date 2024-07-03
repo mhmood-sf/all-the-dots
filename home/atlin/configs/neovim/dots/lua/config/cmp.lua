846408ad8b4c3ebf4efa765230b22dbf3c3bc24f
mhmood-sf <28828855+mhmood-sf@users.noreply.github.com>
2024-07-03 19:07:08 -0400

    fix cmp config

--- home/atlin/configs/neovim/dots/lua/config/cmp.lua
+++ home/atlin/configs/neovim/dots/lua/config/cmp.lua
@@ -1,6 +1,26 @@
-require("cmp").setup {
+local cmp = require("cmp")
+
+cmp.setup {
     sources = {
         { name = "nvim_lsp" },
         { name = "buffer" }
+    },
+
+    mapping = {
+        ['<CR>'] = function(fallback)
+            if cmp.visible() then
+                cmp.confirm()
+            else
+                fallback() -- If you use vim-endwise, this fallback will behave the same as vim-endwise.
+            end
+        end,
+
+        ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end
     }
 }
