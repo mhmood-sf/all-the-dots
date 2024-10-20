bcf5b03219219d14c847804659d03ee732b4a57b
mhmood-sf <28828855+mhmood-sf@users.noreply.github.com>
2024-10-19 23:31:20 -0400

    emacs: add pkg output

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,5 @@
   neovim = pkgs.callPackage ./neovim { };
   iosevka = pkgs.callPackage ./iosevka { };
   tmux = pkgs.callPackage ./tmux { inherit colors; };
+  emacs = pkgs.callPackage ./emacs { };
 }
