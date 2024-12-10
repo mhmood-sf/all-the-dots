cca94daaf6010a6282eca704ca5fa70267e0681e
mhmood-sf <28828855+mhmood-sf@users.noreply.github.com>
2024-12-09 20:18:15 -0500

    update to 24.11

--- host/snowman-ms/system/configuration.nix
+++ host/snowman-ms/system/configuration.nix
@@ -32,22 +32,8 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_CA.UTF-8";
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+  # Persist volume across shutdowns.
+  hardware.alsa.enablePersistence = true;
 
   # Enable bluetooth.
   hardware.bluetooth = {
