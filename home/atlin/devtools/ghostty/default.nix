b046655d578fa1b613e1d4e96673aa835e869923
mhmood-sf <28828855+mhmood-sf@users.noreply.github.com>
2025-01-10 19:23:22 -0500

    ghostty: comments, minor changes

--- home/atlin/devtools/ghostty/default.nix
+++ home/atlin/devtools/ghostty/default.nix
@@ -36,29 +36,44 @@ in {
       font-style-italic = "Medium Italic";
       font-style-bold-italic = "Extrabold Italic";
 
+      # Bigger cell height, with the font centered in the middle, and the
+      # cursor adjusted to the cell's height.
       adjust-cell-height = 5;
+      adjust-cursor-height = 5;
       adjust-font-baseline = -1;
+
+      # Thicker underlines.
       adjust-underline-position = 2;
       adjust-underline-thickness = 2;
 
+      # Slightly thicker bar cursor.
       adjust-cursor-thickness = 2;
+      # Default cursor style, with no blinking.
       cursor-style = "block";
       cursor-style-blink = false;
       # For some reason, shell integration interferes with cursor style.
       shell-integration = "none";
 
+      # Allow window decorations (looks decent enough on GNOME, turn off
+      # when using a window manager with no desktop environment).
       window-decoration = true;
+
+      # Add window padding, where the padded area is colored based on
+      # the current color of the neighbouring cells. Works well when using
+      # TUIs with a different background color.
       window-padding-color = "extend";
       window-padding-x = "7";
       window-padding-y = "7";
 
-      # We'll create the theme based on the provided colors.
+      # We'll create the theme based on the colors provided by the nix config.
       theme = "nixos-ghostty-theme";
     };
     themes = {
       nixos-ghostty-theme = {
+        # Bold font uses bright colors.
         bold-is-bright = true;
         cursor-color = getColor colors.blue;
+        # Just copy over the color scheme.
         foreground = getColor colors.fg-primary;
         background = getColor colors.bg-primary;
         selection-background = getColor palette.aux0;
